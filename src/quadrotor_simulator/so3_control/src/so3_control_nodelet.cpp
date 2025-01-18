6c4d4cdbf75a42c5c88561f3ddacabb504d3b589
Aponia1 <871884904@qq.com>
2025-01-18 08:57:32 -0800

    22354047 黄霆

--- src/quadrotor_simulator/so3_control/src/so3_control_nodelet.cpp
+++ src/quadrotor_simulator/so3_control/src/so3_control_nodelet.cpp
@@ -100,8 +100,8 @@ SO3ControlNodelet::position_cmd_callback(
   des_acc_ = Eigen::Vector3d(cmd->acceleration.x, cmd->acceleration.y,
                              cmd->acceleration.z);
 
-  kx_ = Eigen::Vector3d(5.5, 5.5, 5.5);//
-  kv_ = Eigen::Vector3d(5.5, 5.5, 5.5);
+  kx_ = Eigen::Vector3d(6, 6, 6);//
+  kv_ = Eigen::Vector3d(6, 6, 6);
 
   des_yaw_              = cmd->yaw;
   des_yaw_dot_          = cmd->yaw_dot;
